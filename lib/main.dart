133c37c02c1e18e81be4f7d23281e5fa4f37c330
ParkHyunseo <soilove2@naver.com>
2019-02-08 12:35:26 +0900

    feat: Complete Step 2

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,32 @@ class _HomePageState extends State<HomePage>{
     return Scaffold(      
       body: new Stack(
         children: <Widget>[
-          _buildImage()
+          _buildImage(),
+          _buildTopHeader()
+        ],
+      )
+    );
+  }
+
+  Widget _buildTopHeader() {
+    return new Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
+      child: new Row(
+        children: <Widget>[
+          new Icon(Icons.menu, size:32.0, color: Colors.white),
+          new Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: new Text(
+                "Timeline",
+                style: new TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w300),
+              )
+            ),
+          ),
+          new Icon(Icons.linear_scale, color: Colors.white), 
         ],
       )
     );
