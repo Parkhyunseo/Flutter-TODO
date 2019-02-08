14860938f17b042f274db50e01e56428c9493245
ParkHyunseo <soilove2@naver.com>
2019-02-08 12:46:04 +0900

    feat: Complete Step 3

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,8 @@ class _HomePageState extends State<HomePage>{
       body: new Stack(
         children: <Widget>[
           _buildImage(),
-          _buildTopHeader()
+          _buildTopHeader(),
+          _buildProfileRow()
         ],
       )
     );
@@ -80,6 +81,53 @@ class _HomePageState extends State<HomePage>{
     );
   }
 
+/* static data가 추가되었을 경우 재시작을 해야한다 */
+  Widget _buildProfileRow(){
+    return new Padding(
+      padding: new EdgeInsets.only(
+        left: 16.0,
+        top: _imageHeight / 2.5),
+      child: new Row(
+        children: <Widget>[
+          new CircleAvatar(
+            minRadius: 28.0,
+            maxRadius: 28.0,
+            backgroundImage: new AssetImage('assets/avatar.png'),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left:16.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Text(
+                  'Park Hyunseo',
+                  style: new TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400),
+                ),
+                new Text(
+                  'Game Developer',
+                  style: new TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+/*
+  Widget _buildTodoList()
+  {
+    return Container();
+  }
+*/
   @override
   void dispose() {
     super.dispose();
